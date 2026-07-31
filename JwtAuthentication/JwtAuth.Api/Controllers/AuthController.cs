e5bd52b992e8d5c8d2947978e50c058e26c418a4
Om Zambare <om.zambare@cctech.co.in>
2026-07-31 12:58:05 +0530

    feat: implement user registration API

---
+++ JwtAuthentication/JwtAuth.Api/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using BCrypt.Net;
+using JwtAuth.Api.Data;
+using JwtAuth.Api.DTOs;
+using JwtAuth.Api.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuth.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public AuthController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterRequestDto request)
+    {
+        var existingUser = await _context.Users
+            .FirstOrDefaultAsync(x => x.Email == request.Email);
+
+        if (existingUser != null)
+        {
+            return BadRequest("Email already exists.");
+        }
+
+        var user = new User
+        {
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Email = request.Email,
+            PasswordHash = BCrypt.Net.BCrypt.HashPassword(request.Password),
+            Role = "User"
+        };
+
+        _context.Users.Add(user);
+
+        await _context.SaveChangesAsync();
+
+        return Ok("User registered successfully.");
+    }
+}
---
+++ JwtAuthentication/JwtAuth.Api/DTOs/RegisterRequestDto.cs
@@ -0,0 +1,12 @@
+﻿namespace JwtAuth.Api.DTOs;
+
+public class RegisterRequestDto
+{
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string Password { get; set; } = string.Empty;
+}
